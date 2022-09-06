718929a567b5ff54f1ec953ff91de7a8caaa96bd
guhansenthilkumar <106136892+guhansenthilkumar@users.noreply.github.com>
2022-09-06 13:20:48 +0530

    Add files via upload

---
+++ equaltooperator.js
@@ -0,0 +1,11 @@
+const number= 5;
+const stringNumber='5';
+if(number == stringNumber){
+    console.log("values are equal");
+}
+if(number === stringNumber){
+    console.log("values are equal");
+}
+else{
+    console.log("values are not equal");
+}
