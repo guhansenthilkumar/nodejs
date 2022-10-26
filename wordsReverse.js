a2eef3424be9a1663466ce5ecac685f660beca28
guhansenthilkumar <106136892+guhansenthilkumar@users.noreply.github.com>
2022-10-26 10:14:50 +0530

    Add files via upload

---
+++ wordsReverse.js
@@ -0,0 +1,8 @@
+let str = "My Name Is Vinz";
+str2 = str.split(' ');
+let str3 = "";
+for(let i = 0; i < str2.length; i++){
+    str2[i] = str2[i].split('').reverse().join('');
+    str3 = str3 +" "+ str2[i];
+}
+console.log(str3);
